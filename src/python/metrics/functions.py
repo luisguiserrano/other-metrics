73e241efb31c4eadd71b4b692d8cc59d0a9cae50
luisguiserrano <luisgui.serrano@gmail.com>
2020-09-16 20:07:37 -0400

    small fix

--- src/python/metrics/functions.py
+++ src/python/metrics/functions.py
@@ -1,5 +1,5 @@
 from sklearn.metrics import f1_score
 
 def calculate_f1_score(predictions, labels):
-    f1_score = f1_score(predictions, labels)
-    return f1_score
+    f1 = f1_score(predictions, labels)
+    return f1
